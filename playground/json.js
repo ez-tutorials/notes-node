a506882f44a016adfdbac01efee6ec805a0f40bf
En Zhou <enzhou@MacBookPro.local>
2018-06-13 18:49:47 +0200

    reuse code, json brush up

---
+++ playground/json.js
@@ -0,0 +1,33 @@
+/* var obj = { */
+//   name: 'En'
+// };
+//
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+/* console.log(stringObj); */
+
+//
+// var personString = '{"name": "En", "age": 25}';
+// var person = JSON.parse(personString);
+//
+// console.log(typeof person);
+/* console.log(personString); */
+
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
+
