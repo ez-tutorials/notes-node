538851d362466873b8bbd50aa2ddc7ef2b29c02e
En Zhou <en.zhou@opendna.ai>
2018-06-15 10:39:50 +0200

    updated arrow function

--- playground/arrow-function.js
+++ playground/arrow-function.js
@@ -1,17 +1,16 @@
-
 var square = x  => x * x;
-
 console.log(square(9));
 
 var user = {
   name: 'En',
   sayHi: () => {
+    console.log(arguments);
     console.log(`Hi. I'm ${this.name}`);
   },
   sayHiAlt () {
     console.log(arguments);
     console.log(`Hi. I'm ${this.name}`);
-  }
+  } 
 };
 
-user.sayHi();
+user.sayHi(1,2,3);
